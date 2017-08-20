adec02ec682d0b9b7ea445a52402cafe34f44a64
Thepwarut <thepwarut@gmail.com>
2017-08-20 13:28:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,8 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-			//	'text' => $text
-				'text' => $https://api.anto.io/channel/set/g6WxzaZHiL344vzOljd6yXN2k0VtC6sJlxhUfsiY/NodeMCU/LED1/1
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
