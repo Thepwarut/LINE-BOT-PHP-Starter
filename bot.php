7ad36572deb100a820c9041e1c7f0b6b2a4c5ca8
Thepwarut <thepwarut@gmail.com>
2017-08-20 14:39:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,25 +1,15 @@
 <?php
+$access_token = 'Q9mBh6gIvVPV0WwarYpIJgX2KgaSB8+EVOLQs0ArwbV70v/wy9TSRiDqE5tdWMVBu9f5Hfkhg1IQZo7t75pihBIfiXhf7VSfm4yPB3MzQisRg4pUVXUYgU5ZHhDQP3IoB4Lcw1XntAz8k2Y22GGjCgdB04t89/1O/w1cDnyilFU=';
 
-// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
+$url = 'https://api.line.me/v1/oauth/verify';
 
-include ('line-bot-api/php/line-bot.php');
+$headers = array('Authorization: Bearer ' . $access_token);
 
-$channelSecret = '2955a4a0afede99d8681691bbfea167f';
-$access_token  = 'Q9mBh6gIvVPV0WwarYpIJgX2KgaSB8+EVOLQs0ArwbV70v/wy9TSRiDqE5tdWMVBu9f5Hfkhg1IQZo7t75pihBIfiXhf7VSfm4yPB3MzQisRg4pUVXUYgU5ZHhDQP3IoB4Lcw1XntAz8k2Y22GGjCgdB04t89/1O/w1cDnyilFU=';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
 
-$bot = new BOT_API($channelSecret, $access_token);
-	
-if (!empty($bot->isEvents)) {
-		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
-
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	}
-
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-	exit();
-
-}
+echo $result;
