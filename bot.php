a0409117c7a479307a6af9f8a7b9f36ad6364903
Thepwarut <thepwarut@gmail.com>
2017-08-20 16:40:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'หวัดดี',
-				'text' => $ยินดีต้อนรับ
+				'text' => $'ยินดีต้อนรับ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
