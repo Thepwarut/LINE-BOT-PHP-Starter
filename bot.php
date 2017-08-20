fde149778b766542bfbf31f8fa96d7fc95e8966d
Thepwarut <thepwarut@gmail.com>
2017-08-20 19:54:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ else if($arrJson['events'][0]['message']['text'] == "เปิดไฟ"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $https://api.anto.io/channel/set/g6WxzaZHiL344vzOljd6yXN2k0VtC6sJlxhUfsiY/NodeMCU/LED1/1;
+  $arrPostData['messages'][0]['text'] = 'https://api.anto.io/channel/set/g6WxzaZHiL344vzOljd6yXN2k0VtC6sJlxhUfsiY/NodeMCU/LED1/1';
 }
 
 else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
