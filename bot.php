7d4707261694183b6d1962a84c2d98d512077351
Thepwarut <thepwarut@gmail.com>
2017-08-20 13:15:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,8 +26,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-			//	'messages' => [$messages],
-			$url =	https://api.anto.io/channel/set/g6WxzaZHiL344vzOljd6yXN2k0VtC6sJlxhUfsiY/NodeMCU/LED1/1,
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
